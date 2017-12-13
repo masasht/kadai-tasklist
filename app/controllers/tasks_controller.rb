98a3de42115d4d64ccc06fb56c89048649c0f8ef
kano-pon <masasht@gmail.com>
2017-12-13 14:57:16 +0000

    課題提出　1回目

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = task.new(task_params)
+    @task = Task.new(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に追加されました'
@@ -30,7 +30,7 @@ class TasksController < ApplicationController
   def update
      @task = Task.find(params[:id])
 
-    if @task.update(message_params)
+    if @task.update(task_params)
       flash[:success] = 'タスク は正常に更新されました'
       redirect_to @task
     else
