1aa57a28bc17268f7aff77a48f4c9796d2f7cb70
kano-pon <masasht@gmail.com>
2017-12-15 06:34:37 +0000

    add column

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
-  validates :content, presence: true, length: { maximum: 255 }
+  validates :content, presence: {message: "タスクを入力してね"}, length: { maximum: 255, :message => "長すぎるよ！255文字以内にしてね" }
+  validates :status, presence: {message: "ステータスを選択してね"}, length: { maximum: 10 }
 end
