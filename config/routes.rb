47993ded293f369d50ad62f9ac3cb116d575bed7
kano-pon <masasht@gmail.com>
2017-12-22 00:45:54 +0000

    ユーザ登録　ログイン

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,22 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'users/new'
+
+  get 'users/create'
+
   root to: 'tasks#index'
   
   get 'signup', to: 'users#new'
-  resources :users
+  resources :users, only: [:new, :create]
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   resources :tasks
   
